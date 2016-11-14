56c8cb7af5f529d581ed455a45c5defd2c47063a
HuangXiaoLong <1150189655@qq.com>
2016-11-14 19:05:11 +0800

    get images

--- getimages.py
+++ getimages.py
@@ -1,11 +1,18 @@
+import time
 import os
 import re
 
 f = open("username","r")
 image_urlformat = 'http://codeforces.com/userphoto/title/%s/photo.jpg'
 
-#for username in f.readlines():
-username = f.readline()
-username = username.strip('\n')
-os.system('wget "%s" --output-document="%s"' % (image_urlformat % username, './images/%s.jpg' % username))
+if not os.path.exists("images"):
+    os.makedirs("images")
+
+cnt = 0
+for username in f.readlines():
+    username = username.strip('\n')
+    cnt += 1
+    print "fetching user%d: %s's Avatar" % (cnt, username)
+    os.system('wget "%s" --quiet --output-document="%s"' % (image_urlformat % username, './images/%s.jpg' % username))
+    time.sleep(0.1)
 
